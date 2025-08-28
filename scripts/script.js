8bf346668bb0761271b4eb2e0ea4ac4cf151f606
jamiajahan <jaamnishan@gmail.com>
2025-08-29 00:53:52 +0600

    my commit 3

--- scripts/script.js
+++ scripts/script.js
@@ -67,8 +67,3 @@ document.querySelectorAll(".callBtn").forEach((btn) => {
     }
   });
 });
-
-// --- CLEAR HISTORY ---
-clearHistoryBtn.addEventListener("click", () => {
-  historyList.innerHTML = "";
-});
